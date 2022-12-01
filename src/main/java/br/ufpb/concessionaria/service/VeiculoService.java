a086a4ad7890f558a55d728719f319d4b7816bb6
VLV01 <vinicius.linsvasco@hotmail.com>
2022-12-01 10:37:16 -0300

    VeiculoService, VendedorService

--- src/main/java/br/ufpb/concessionaria/service/VeiculoService.java
+++ src/main/java/br/ufpb/concessionaria/service/VeiculoService.java
@@ -1,4 +1,55 @@
 package br.ufpb.concessionaria.service;
 
+import br.ufpb.concessionaria.models.Veiculo;
+import br.ufpb.concessionaria.repository.VeiculoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class VeiculoService {
+    VeiculoRepository veiculoRepository;
+
+    public VeiculoService(VeiculoRepository veiculoRepository) {
+        this.veiculoRepository = veiculoRepository;
+    }
+
+    public Veiculo createVeiculo(Veiculo veiculo){
+        return veiculoRepository.save(veiculo);
+    }
+
+    public List<Veiculo> listVeiculo(){
+        return veiculoRepository.findAll();
+    }
+
+    public Veiculo getVeiculo(Long veiculoId){
+        Optional<Veiculo> optionalVeiculo = veiculoRepository.findById(veiculoId);
+        if (optionalVeiculo.isPresent()){
+            return veiculoRepository.getReferenceById(veiculoId);
+        }
+        return null;
+    }
+    public Veiculo updateVeiculo(Long veiculoId, Veiculo veiculo){
+        Optional<Veiculo> optionalVeiculo = veiculoRepository.findById(veiculoId);
+        if (optionalVeiculo.isPresent()){
+            Veiculo toUpdate = optionalVeiculo.get();
+            toUpdate.setChassi(veiculo.getChassi());
+            toUpdate.setModelo(veiculo.getModelo());
+            toUpdate.setNome(veiculo.getNome());
+            toUpdate.setAno(veiculo.getAno());
+            toUpdate.setCor(veiculo.getCor());
+            return toUpdate;
+        }
+        return null;
+    }
+
+
+    public Veiculo deleteVeiculo(Long veiculoId){
+        Optional<Veiculo> optionalVeiculo = veiculoRepository.findById(veiculoId);
+        if (optionalVeiculo.isPresent()){
+            veiculoRepository.deleteById(veiculoId);
+        }
+        return null;
+    }
 }
--- src/main/java/br/ufpb/concessionaria/service/VendaService.java
+++ src/main/java/br/ufpb/concessionaria/service/VendaService.java
@@ -1,5 +1,53 @@
 package br.ufpb.concessionaria.service;
 
-public class VendaService {
+import br.ufpb.concessionaria.models.Venda;
+import br.ufpb.concessionaria.repository.VendaRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VendaService {
+    VendaRepository vendaRepository;
+
+    public VendaService(VendaRepository vendaRepository) {
+        this.vendaRepository = vendaRepository;
+    }
+
+    public Venda createVenda(Venda venda){
+        return vendaRepository.save(venda);
+    }
+
+    public List<Venda> listVenda(){
+        return vendaRepository.findAll();
+    }
+
+    public Venda getVenda(Long vendaId){
+        Optional<Venda> optionalVenda = vendaRepository.findById(vendaId);
+        if (optionalVenda.isPresent()){
+            return vendaRepository.getReferenceById(vendaId);
+        }
+        return null;
+    }
+    public Venda updateVenda(Long vendaId, Venda venda){
+        Optional<Venda> optionalVenda = vendaRepository.findById(vendaId);
+        if (optionalVenda.isPresent()){
+            Venda toUpdate = optionalVenda.get();
+            toUpdate.setClienteId(venda.getClienteId());
+            toUpdate.setVendedorId(venda.getVendedorId());
+            toUpdate.setVeiculoId(venda.getVeiculoId());
+
+            return toUpdate;
+        }
+        return null;
+    }
+
+    public Venda deleteVenda(Long vendaId){
+        Optional<Venda> optionalVenda = vendaRepository.findById(vendaId);
+        if (optionalVenda.isPresent()){
+            vendaRepository.deleteById(vendaId);
+        }
+        return null;
+    }
 }
