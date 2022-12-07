95528f4b6a8c60a1daa70a350a8a6718848693a4
VLV01 <vinicius.linsvasco@hotmail.com>
2022-12-06 21:54:13 -0300

    Load

--- src/main/java/br/ufpb/concessionaria/load/Load.java
+++ src/main/java/br/ufpb/concessionaria/load/Load.java
@@ -20,35 +20,113 @@ public class Load {
 
     public void carregarCliente(){
         Cliente cliente = new Cliente();
+        Cliente cliente2 = new Cliente();
+        Cliente cliente3 = new Cliente();
+        Cliente cliente4 = new Cliente();
+        Cliente cliente5 = new Cliente();
 
-        cliente.setNome("Exemplo");
+        cliente.setNome("Luiz da Silva");
         cliente.setCpf("399-498-628-39");
-        cliente.setEmail("exemplo@gmail.com");
-        cliente.setCnh("0123456789");
+        cliente.setEmail("Luiz@gmail.com");
+        cliente.setCnh("9323956999");
+        cliente2.setNome("Carlos Alberto");
+        cliente2.setCpf("976-947-673-10");
+        cliente2.setEmail("carlos@gmail.com");
+        cliente2.setCnh("0197643904");
+        cliente3.setNome("Roberto Palmeira");
+        cliente3.setCpf("672-637-974-37");
+        cliente3.setEmail("roberto@gmail.com");
+        cliente3.setCnh("7964380193");
+        cliente4.setNome("André Vargas");
+        cliente4.setCpf("319-439-193-88");
+        cliente4.setEmail("andre@gmail.com");
+        cliente4.setCnh("9734601437");
+        cliente5.setNome("José Clemente");
+        cliente5.setCpf("932-246-763-11");
+        cliente5.setEmail("jose@gmail.com");
+        cliente5.setCnh("3764928346");
         clienteRepository.save(cliente);
+        clienteRepository.save(cliente2);
+        clienteRepository.save(cliente3);
+        clienteRepository.save(cliente4);
+        clienteRepository.save(cliente5);
 
     }
 
+
+
     public void carregarVendedor(){
         Vendedor vendedor = new Vendedor();
+        Vendedor vendedor2 = new Vendedor();
+        Vendedor vendedor3 = new Vendedor();
+        Vendedor vendedor4 = new Vendedor();
+        Vendedor vendedor5 = new Vendedor();
 
-        vendedor.setNome("Exemplo da Silva");
-        vendedor.setCpf("399-498-628-39");
-        vendedor.setEmail("exemplo@gmail.com");
-        vendedor.setSalario("0123456789");
+        vendedor.setNome("Januário Guimarães");
+        vendedor.setCpf("943-317-973-37");
+        vendedor.setEmail("januario@gmail.com");
+        vendedor.setSalario("7349831604");
+        vendedor2.setNome("Kevin Freire");
+        vendedor2.setCpf("768-197-376-76");
+        vendedor2.setEmail("kevin@gmail.com");
+        vendedor2.setSalario("7349700197");
+        vendedor3.setNome("Julio Paulo");
+        vendedor3.setCpf("221-976-379-77");
+        vendedor3.setEmail("julio@gmail.com");
+        vendedor3.setSalario("4337946834");
+        vendedor4.setNome("Raul Alburquerque");
+        vendedor4.setCpf("943-649-777-666");
+        vendedor4.setEmail("raul@gmail.com");
+        vendedor4.setSalario("9731887694");
+        vendedor5.setNome("Tiago Beltrão");
+        vendedor5.setCpf("734-493-197-99");
+        vendedor5.setEmail("tiago@gmail.com");
+        vendedor5.setSalario("0055937834");
         vendedorRepository.save(vendedor);
+        vendedorRepository.save(vendedor2);
+        vendedorRepository.save(vendedor3);
+        vendedorRepository.save(vendedor4);
+        vendedorRepository.save(vendedor5);
 
     }
 
     public void carregarVeiculo(){
         Veiculo veiculo = new Veiculo();
+        Veiculo veiculo2 = new Veiculo();
+        Veiculo veiculo3 = new Veiculo();
+        Veiculo veiculo4 = new Veiculo();
+        Veiculo veiculo5 = new Veiculo();
 
         veiculo.setChassi("0123456789");
-        veiculo.setModelo("Etios");
-        veiculo.setNome("Exemplo da Silva");
+        veiculo.setModelo("Hatch");
+        veiculo.setNome("Neo");
         veiculo.setAno("2016");
-        veiculo.setCor("Branco");
+        veiculo.setCor("Branca");
+        veiculo.setChassi("0123456789");
+        veiculo.setModelo("Sedan");
+        veiculo.setNome("T20");
+        veiculo.setAno("2019");
+        veiculo.setCor("Pickup");
+        veiculo.setChassi("0123456789");
+        veiculo.setModelo("SUV");
+        veiculo.setNome("Osiris");
+        veiculo.setAno("2014");
+        veiculo.setCor("Prata");
+        veiculo.setChassi("0123456789");
+        veiculo.setModelo("Cabine");
+        veiculo.setNome("Cara-cara");
+        veiculo.setAno("2012");
+        veiculo.setCor("Azul");
+        veiculo.setChassi("0123456789");
+        veiculo.setModelo("Cabine Dupla");
+        veiculo.setNome("ItaliRXS");
+        veiculo.setAno("2010");
+        veiculo.setCor("Amarela");
         veiculoRepository.save(veiculo);
+        veiculoRepository.save(veiculo2);
+        veiculoRepository.save(veiculo3);
+        veiculoRepository.save(veiculo4);
+        veiculoRepository.save(veiculo5);
 
     }
 
